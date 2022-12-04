c5d9ee0775f124e448bb4ffdb1ffdec6615d1054
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-04 05:42:47 -0800

    Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3
+
+// Напишите программу, которая 
+// принимает на вход число (N) и выдаёт таблицу кубов 
+// чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.WriteLine($"{message} ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Enter a number");
+for(int i = 1; i <= number; i++)
+{
+   int cube = i * i * i;
+   Console.WriteLine($"{cube}");
+}
+
+
+
